72bdaeedcb5c79d648d1c66fc4173b030f66c842
Ritesh Pandey <riteshpandey427@gmail.com>
2026-03-04 20:55:44 +0530

    Initial commit.

---
+++ src/main/java/com/natwest/com/traffic_light_controller_api/Application.java
@@ -0,0 +1,13 @@
+package com.natwest.com.traffic_light_controller_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/natwest/com/traffic_light_controller_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natwest.com.traffic_light_controller_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
